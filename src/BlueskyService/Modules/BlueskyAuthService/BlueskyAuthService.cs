f464c60136e0da9a30a4393615441536cf644ddc
Elys Maldov <maldovelys@gmail.com>
2026-02-21 23:54:00 +0700

    feat(BlueskyService): add multithreading safety to RefreshSession

--- src/BlueskyService/Modules/BlueskyAuthService/BlueskyAuthService.cs
+++ src/BlueskyService/Modules/BlueskyAuthService/BlueskyAuthService.cs
@@ -35,22 +35,59 @@ public class BlueskyAuthService(HttpClient httpClient) : IBlueskyAuthService
     return session;
   }
 
+  private readonly SemaphoreSlim _refreshSemaphore = new(1, 1);
+  private BlueskySession? _cachedRefreshedSession;
+
+  /// <summary>
+  /// Uses semaphore for multithreading safety to limit if multiple
+  /// request gets 401 so we only need to refresh once, and the other failed
+  /// requests can instantly retry using the latest refresh tokens.
+  ///
+  /// Multithread safety is required here since we may get multiple concurrent request
+  /// where each request is handled by a different thread which activate this method
+  /// from the delegating handler multiple times.
+  ///
+  /// Example: if 4 requests fails 401, the first request will take the semaphore,
+  /// while the other 3 will wait for it to go back. The first request will
+  /// get the new refresh token, cache it, return the semaphore, the other request
+  /// continue the request, sees there is already a cached refresh session, and instantly
+  /// use the cached one instead of making 3 requests again
+  /// </summary>
+  /// <param name="refreshToken"></param>
+  /// <returns></returns>
   public async Task<BlueskySession> RefreshSession(string refreshToken)
   {
-    var endpoint = "/xrpc/com.atproto.server.refreshSession";
+    await _refreshSemaphore.WaitAsync();
 
-    // We need to manually create a seperate request to override the auth header only for this request
-    using var request = new HttpRequestMessage(HttpMethod.Post, endpoint);
-    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", refreshToken);
+    try
+    {
+      // If the 'refreshToken' we just received is DIFFERENT from the one currently in our
+      // system/store, it means another thread has already refreshed it.
+      if (_cachedRefreshedSession.HasValue && _cachedRefreshedSession.Value.RefreshToken != refreshToken)
+      {
+        return _cachedRefreshedSession.Value;
+      }
 
-    using HttpResponseMessage response = await httpClient.SendAsync(request);
+      var endpoint = "/xrpc/com.atproto.server.refreshSession";
 
-    response.EnsureSuccessStatusCode();
+      // We need to manually create a seperate request to override the auth header only for this request
+      using var request = new HttpRequestMessage(HttpMethod.Post, endpoint);
+      request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", refreshToken);
 
-    var jsonResponse = await response.Content.ReadAsStringAsync();
-    var sessionDto = SessionDto.FromJson(jsonResponse);
-    var session = sessionDto.ToDomain();
+      using HttpResponseMessage response = await httpClient.SendAsync(request);
 
-    return session;
+      response.EnsureSuccessStatusCode();
+
+      var jsonResponse = await response.Content.ReadAsStringAsync();
+      var sessionDto = SessionDto.FromJson(jsonResponse);
+      var session = sessionDto.ToDomain();
+      _cachedRefreshedSession = session;
+
+      return session;
+    }
+    finally
+    {
+      _refreshSemaphore.Release();
+    }
   }
 }
