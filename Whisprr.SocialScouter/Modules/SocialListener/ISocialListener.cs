f26ab652c30646e953e2bed4d489b0eae3a64c6a
Elys Maldov <maldovelys@gmail.com>
2026-02-21 21:52:47 +0700

    feat(SocialScouter): create SocialListener abstract class

--- Whisprr.SocialScouter/Modules/SocialListener/ISocialListener.cs
+++ Whisprr.SocialScouter/Modules/SocialListener/ISocialListener.cs
@@ -2,7 +2,11 @@ using Whisprr.Entities.Models;
 
 namespace Whisprr.SocialScouter.Modules.SocialListener;
 
-interface ISocialListener
+/// <summary>
+/// I still use an interface here for easier Dependency Injection in our program
+/// and tests.
+/// </summary>
+public interface ISocialListener
 {
   public Task<SocialInfo[]> Search(SocialTopicListeningTask task);
 }
