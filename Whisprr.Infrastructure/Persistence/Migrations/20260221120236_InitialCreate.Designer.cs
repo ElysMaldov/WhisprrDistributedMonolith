71c04ac23e9e18cb0a48576281e127c762de44df
Elys Maldov <maldovelys@gmail.com>
2026-02-21 19:15:16 +0700

    build: setup makefile for migrations

--- Whisprr.Infrastructure/Persistence/Migrations/20260221120236_InitialCreate.Designer.cs
+++ Whisprr.Infrastructure/Persistence/Migrations/20260221120236_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Whisprr.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using Whisprr.Infrastructure.Persistence;
 namespace Whisprr.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260221120236_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
